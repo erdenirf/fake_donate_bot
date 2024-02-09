3d6ab6afe395920109ca3fa66b04bd48407dd35d
Erdeni Tsyrendashiev <erdenirf@gmail.com>
2024-02-09 08:14:46 +0300

    consumer

---
+++ client_broker_consumer.py
@@ -0,0 +1,21 @@
+from broker_rabbitmq import Broker
+from config_reader import config
+
+broker = Broker()
+
+channel = broker.channel
+
+# Функция, которая будет вызвана при получении сообщения
+def callback(ch, method, properties, body):
+    bodyMessage = body.decode('utf-8')
+    print(f"Received: '{bodyMessage}'")
+
+# Подписка на очередь и установка обработчика сообщений
+channel.basic_consume(
+    queue=config.rabbitmq_topicname,
+    on_message_callback=callback,
+    auto_ack=True  # Автоматическое подтверждение обработки сообщений
+)
+
+print('Waiting for messages. To exit, press Ctrl+C')
+channel.start_consuming()
